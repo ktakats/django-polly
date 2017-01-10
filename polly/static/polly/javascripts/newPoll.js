239c6ba20db8a75da295218b3fe0297a15f21adc
ktakats <ktakats@gmail.com>
2017-01-10 19:07:35 -0300

    Remove options button works

--- polly/static/polly/javascripts/newPoll.js
+++ polly/static/polly/javascripts/newPoll.js
@@ -1,12 +1,28 @@
 $(document).ready(function(){
+numoption=2;
+
+  $(document).on("click", ".btn-remove", function(e){
+    e.preventDefault();
+    var toremove=$(this).parents(".form-group");
+    $(toremove).remove();
+    count=Number($('#id_option_count').val());
+    count-=1;
+    $('#id_option_count').val(count);
+  })
 
   $(document).on("click", ".btn-add", function(e){
     e.preventDefault();
     count=Number($('#id_option_count').val());
+    numoption+=1
     var tocopy=$(this).parents(".form-group");
     var current=$(tocopy).parents(".input-group");
     var newEntry=$(tocopy).clone().appendTo(current);
-    newEntry.find('input').attr("id", "id_option_"+count).attr("name", "option_"+count).attr('placeholder', "Option "+String(Number(count)+1));
+    newEntry.find('input').attr("id", "id_option_"+String(numoption-1)).attr("name", "option_"+String(numoption-1)).attr('placeholder', "Option "+String(numoption));
+
+    $(this).removeClass("btn-add").addClass("btn-remove")
+    $(this)[0].innerHTML="-"
+    console.log($(this)[0].innerHTML)
+
 
   count+=1;
   $('#id_option_count').val(count);
